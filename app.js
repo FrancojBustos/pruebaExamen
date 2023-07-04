687a2ec315cae4b3cb3ed8cb6cdc2c6f8fe313be
FrancojBustos <bustosfranco061@gmail.com>
2023-07-04 10:55:28 -0300

    hola 2

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
-
-const path = require('path');
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
 // Middlewares
 // TODO: Implementar middlewares
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(corse());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
-
+app.use((req, res, next) => {
+  return res.status(404).render("404");
+});
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`server corriendo en el puerto ${port}`));
